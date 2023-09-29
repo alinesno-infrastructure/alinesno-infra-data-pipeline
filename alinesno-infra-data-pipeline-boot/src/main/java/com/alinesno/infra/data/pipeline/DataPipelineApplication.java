106e64f14f014bb81baa2a7148585a4dd064d154
switchII <luoandon@gmail.com>
2023-09-29 19:59:28 +0800

    fix:处理域名不正确的问题

--- alinesno-infra-data-pipeline-boot/src/main/java/com/alinesno/infra/data/pipeline/DataPipelineApplication.java
+++ alinesno-infra-data-pipeline-boot/src/main/java/com/alinesno/infra/data/pipeline/DataPipelineApplication.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.data.pipeline;
 
 import com.alinesno.infra.common.core.context.SpringContext;
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.data.pipeline.mapper")
 @SpringBootApplication
 public class DataPipelineApplication {

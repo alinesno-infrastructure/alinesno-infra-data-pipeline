6d731971d524f466a6d732038380e65b22907518
王磊(Levi) <leviAip@gmail.com>
2024-08-12 08:58:00 -0400

    新增初始化服务接口IPipelineInitService及其初始化调度器方法。

---
+++ alinesno-infra-data-pipeline-initialize/src/main/java/com/alinesno/infra/data/pipeline/initialize/IPipelineInitService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.data.pipeline.initialize;
+
+/**
+ * 初始化服务接口
+ */
+public interface IPipelineInitService {
+
+    /**
+     * 初始化调度器
+     */
+    void initScheduler();
+
+}

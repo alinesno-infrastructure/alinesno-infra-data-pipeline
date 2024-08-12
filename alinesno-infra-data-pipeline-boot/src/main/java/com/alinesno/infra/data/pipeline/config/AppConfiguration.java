c3673abd60cdbbaa74efe95bc4b9ddf90cfaaff7
王磊(Levi) <leviAip@gmail.com>
2024-08-11 21:31:00 -0400

    修复并增强AppConfiguration配置类，添加定时任务初始化和日志切面注解。

--- alinesno-infra-data-pipeline-boot/src/main/java/com/alinesno/infra/data/pipeline/config/AppConfiguration.java
+++ alinesno-infra-data-pipeline-boot/src/main/java/com/alinesno/infra/data/pipeline/config/AppConfiguration.java
@@ -2,9 +2,13 @@ package com.alinesno.infra.data.pipeline.config;
 
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.alinesno.infra.data.pipeline.initialize.IPipelineInitService;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -20,9 +24,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration implements CommandLineRunner {
 
+    @Autowired
+    private IPipelineInitService pipelineInitService ;
+
+    @Bean
+    public LogAspect getLogAspect(){
+        return new LogAspect() ;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        pipelineInitService.initScheduler(); // 初始化定时任务
     }
 
 }

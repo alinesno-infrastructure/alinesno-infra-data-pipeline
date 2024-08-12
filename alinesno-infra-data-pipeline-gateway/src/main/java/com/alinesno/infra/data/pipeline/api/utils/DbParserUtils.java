b5a8347de529d19f39fde50ae40f3a9d60af61d2
王磊(Levi) <leviAip@gmail.com>
2024-08-12 01:24:00 -0400

    feat:实现数据库连接验证功能，解析MySQL JDBC链接信息

---
+++ alinesno-infra-data-pipeline-gateway/src/main/java/com/alinesno/infra/data/pipeline/api/utils/DbParserUtils.java
@@ -0,0 +1,63 @@
+package com.alinesno.infra.data.pipeline.api.utils;
+
+import com.alinesno.infra.data.pipeline.entity.DatasourceEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * 数据库连接验证
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+@Slf4j
+public class DbParserUtils {
+
+	/**
+	 * 解析mysql jdbc链接
+	 * @param jdbcUrl
+	 */
+	public static void parserJdbcUrl(DatasourceEntity e , String jdbcUrl) {
+		
+		String pattern="jdbc:(?<type>[a-z]+)://(?<host>[a-zA-Z0-9-//.]+):(?<port>[0-9]+)/(?<database>[a-zA-Z0-9_]+)?";
+        Pattern namePattern = Pattern.compile(pattern);
+        Matcher dateMatcher = namePattern.matcher(jdbcUrl);
+        
+		String dbType = null ; 
+		String host = null ; 
+		String port = null ; 
+		String database = null ; 
+		String query = jdbcUrl.substring(jdbcUrl.indexOf("?") + 1); 
+        
+        while (dateMatcher.find()) {
+        	
+            dbType = dateMatcher.group("type");
+            host = dateMatcher.group("host");
+            port = dateMatcher.group("port");
+            database = dateMatcher.group("database");
+            
+            log.debug("dbType = {}" , dbType);
+            log.debug("host = {}" , host);
+            log.debug("port = {}" , port);
+            log.debug("database = {}" , database);
+            log.debug("query = {}" , query);
+
+        }
+        
+        Assert.hasLength(dbType , "数据库类型为空");
+        Assert.hasLength(host, "数据IP类型为空");
+        Assert.hasLength(port , "数据端口类型为空");
+        Assert.hasLength(database , "数据库名类型为空");
+       
+        e.setDbPort(port);
+        e.setDbName(database) ; 
+        e.setDbUrl(host) ; 
+        e.setJdbcUrl(jdbcUrl); 
+        
+	}
+
+}

2d186268340d04930522774aeb27cd8ede26b36a
周健(James) <jamesAip@gmail.com>
2024-08-14 10:20:00 -0400

    修复SourceReader类字段类型错误，将String id修改为long id。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/SourceReader.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/SourceReader.java
@@ -1,48 +1,21 @@
 package com.alinesno.infra.data.pipeline.scheduler.dto;
 
 import com.alinesno.infra.data.pipeline.scheduler.enums.SourceReaderEnums;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 读取端
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class SourceReader extends BaseParams  {
 
-    private String id ; // 唯一标识
+    private long id ; // 唯一标识
     private String type = SourceReaderEnums.MYSQL.getCode() ;
     private String name ;
 
     // 查询SQL
     private String querySql ;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getQuerySql() {
-        return querySql;
-    }
-
-    public void setQuerySql(String querySql) {
-        this.querySql = querySql;
-    }
 }

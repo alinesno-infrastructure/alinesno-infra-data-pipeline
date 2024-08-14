9672d2fdcb34e3d92759f7a5074e8db300bee3c2
张婷(Tina) <tinaAip@gmail.com>
2024-08-14 08:51:00 -0400

    新增 ReaderSourceMapper 接口文件。

---
+++ alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/mapper/ReaderSourceMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.pipeline.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.pipeline.entity.ReaderSourceEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ReaderSourceMapper extends IBaseMapper<ReaderSourceEntity> {
+
+}

6b778462951b2480679322e847dbb97e48e243fc
张婷(Tina) <tinaAip@gmail.com>
2024-08-13 23:31:00 -0400

    优化Pipeline初始化逻辑，新增读取源示例初始化功能。

--- alinesno-infra-data-pipeline-initialize/src/main/java/com/alinesno/infra/data/pipeline/initialize/impl/PipelineInitServiceImpl.java
+++ alinesno-infra-data-pipeline-initialize/src/main/java/com/alinesno/infra/data/pipeline/initialize/impl/PipelineInitServiceImpl.java
@@ -1,9 +1,12 @@
 package com.alinesno.infra.data.pipeline.initialize.impl;
 
+import com.alinesno.infra.data.pipeline.entity.ReaderSourceEntity;
 import com.alinesno.infra.data.pipeline.entity.JobEntity;
 import com.alinesno.infra.data.pipeline.initialize.IPipelineInitService;
 import com.alinesno.infra.data.pipeline.initialize.builder.DataInitializer;
+import com.alinesno.infra.data.pipeline.initialize.builder.DataSourceSamples;
 import com.alinesno.infra.data.pipeline.service.IJobService;
+import com.alinesno.infra.data.pipeline.service.IReaderSourceService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 初始化服务实现类
@@ -28,6 +32,9 @@ public class PipelineInitServiceImpl implements IPipelineInitService {
 
     @Autowired
     private IJobService jobService ;
+
+    @Autowired
+    private IReaderSourceService readerSourceService ;
 
     @SneakyThrows
     @Override
@@ -44,5 +51,23 @@ public class PipelineInitServiceImpl implements IPipelineInitService {
     public void initDemoJob() {
         List<JobEntity> jobs = DataInitializer.initializeJobs() ;
         jobService.saveOrUpdateBatch(jobs) ;
+    }
+
+    @Override
+    public void initReaderSource() {
+        List<ReaderSourceEntity> sampleDataSources = DataSourceSamples.createSampleDataSources();
+
+        AtomicLong id = new AtomicLong(1L);
+        sampleDataSources.forEach(e -> {
+            e.setId(id.getAndIncrement());
+            if(id.get()%3 == 0){
+                e.setSourceType("source");
+            }else{
+                e.setSourceType("sink");
+            }
+        }) ;
+
+        readerSourceService.saveOrUpdateBatch(sampleDataSources) ;
+        log.info("初始化读取源示例成功");
     }
 }

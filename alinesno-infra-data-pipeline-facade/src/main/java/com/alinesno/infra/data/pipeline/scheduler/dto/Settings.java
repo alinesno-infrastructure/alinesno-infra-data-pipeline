ea861345ae75b71e0df7996de412b59072f84fe5
周健(James) <jamesAip@gmail.com>
2024-08-14 09:46:00 -0400

    修复并添加Lombok注解至Settings类以提升代码可读性。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/Settings.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/Settings.java
@@ -1,37 +1,17 @@
 package com.alinesno.infra.data.pipeline.scheduler.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * 配置信息
  */
+@Data
 public class Settings {
 
     private String cron ;
     private Date startTime ;
     private Date endTime ;
 
-    public String getCron() {
-        return cron;
-    }
-
-    public void setCron(String cron) {
-        this.cron = cron;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
 }

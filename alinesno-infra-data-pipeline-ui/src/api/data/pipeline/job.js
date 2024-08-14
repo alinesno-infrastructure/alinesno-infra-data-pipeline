0f2a76b1e69059e19f015ec8766845afcf889acc
周健(James) <jamesAip@gmail.com>
2024-08-14 09:43:00 -0400

    创建 job API 接口文件

---
+++ alinesno-infra-data-pipeline-ui/src/api/data/pipeline/job.js
@@ -0,0 +1,86 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/data/pipeline/job/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    listAllJob: prefix + "listAllJob",
+    downloadfile: prefix + "downloadfile"
+}
+
+// 列出所有集成渠道
+export function listAllJob(){
+  return request({
+    url: managerUrl.listAllJob ,
+    method: 'get'
+  })
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询数据库列表
+export function listJob(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getJob(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addJob(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改数据库
+export function updateJob(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除数据库
+export function delJob(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}

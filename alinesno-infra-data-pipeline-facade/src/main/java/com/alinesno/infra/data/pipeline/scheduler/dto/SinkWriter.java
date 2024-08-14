4005c10e16f336eb31e5d893bf07de05df67118e
周健(James) <jamesAip@gmail.com>
2024-08-14 06:34:00 -0400

    修复：调整SinkWriter类的id属性为long类型。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/SinkWriter.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/dto/SinkWriter.java
@@ -1,47 +1,20 @@
 package com.alinesno.infra.data.pipeline.scheduler.dto;
 
 import com.alinesno.infra.data.pipeline.scheduler.enums.SourceReaderEnums;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 写入端
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class SinkWriter extends BaseParams {
 
-    private String id ; // 唯一标识
+    private long id ; // 唯一标识
     private String type = SourceReaderEnums.MYSQL.getCode() ;
     private String name ;
 
     private String writeModel ; // insert/replace
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getWriteModel() {
-        return writeModel;
-    }
-
-    public void setWriteModel(String writeModel) {
-        this.writeModel = writeModel;
-    }
 }

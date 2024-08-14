c0fcd8be49b4db344a8025152ec41afe9a8d72b7
周健(James) <jamesAip@gmail.com>
2024-08-14 09:29:00 -0400

    修复并扩展初始化服务实现类，添加定时任务和示例作业初始化功能。

--- alinesno-infra-data-pipeline-initialize/src/main/java/com/alinesno/infra/data/pipeline/initialize/impl/PipelineInitServiceImpl.java
+++ alinesno-infra-data-pipeline-initialize/src/main/java/com/alinesno/infra/data/pipeline/initialize/impl/PipelineInitServiceImpl.java
@@ -1,6 +1,9 @@
 package com.alinesno.infra.data.pipeline.initialize.impl;
 
+import com.alinesno.infra.data.pipeline.entity.JobEntity;
 import com.alinesno.infra.data.pipeline.initialize.IPipelineInitService;
+import com.alinesno.infra.data.pipeline.initialize.builder.DataInitializer;
+import com.alinesno.infra.data.pipeline.service.IJobService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * 初始化服务实现类
@@ -22,6 +26,9 @@ public class PipelineInitServiceImpl implements IPipelineInitService {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private IJobService jobService ;
+
     @SneakyThrows
     @Override
     public void initScheduler() {
@@ -31,5 +38,11 @@ public class PipelineInitServiceImpl implements IPipelineInitService {
         ScriptUtils.executeSqlScript(dataSource.getConnection(), encodedResource);
 
         log.info("初始化定时任务成功");
+    }
+
+    @Override
+    public void initDemoJob() {
+        List<JobEntity> jobs = DataInitializer.initializeJobs() ;
+        jobService.saveOrUpdateBatch(jobs) ;
     }
 }

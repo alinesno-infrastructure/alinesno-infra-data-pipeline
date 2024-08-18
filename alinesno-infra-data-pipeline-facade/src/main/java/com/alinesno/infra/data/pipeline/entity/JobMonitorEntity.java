64dcb60982bd1d2463b36108d10a94aea4867ed2
李伟(Michael) <michaelAip@gmail.com>
2024-08-17 22:07:00 -0400

    修复并优化JobMonitorEntity类的字段访问控制及文档注释

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/entity/JobMonitorEntity.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/entity/JobMonitorEntity.java
@@ -1,10 +1,12 @@
 package com.alinesno.infra.data.pipeline.entity;
 
-import java.util.Date;
-
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
 
 /**
  * 功能名：数据抽取任务监控
@@ -17,6 +19,8 @@ import com.baomidou.mybatisplus.annotation.TableName;
  * @author luoxiaodong
  * @version 1.0.0
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 @TableName("pipeline_job_monitor")
 public class JobMonitorEntity extends InfraBaseEntity {
 
@@ -62,101 +66,4 @@ public class JobMonitorEntity extends InfraBaseEntity {
     @TableField("next_execute_time")
     private Date nextExecuteTime;
 
-    /**
-     * 获取监控任务ID
-     */
-    public String getMonitorJobId() {
-        return monitorJobId;
-    }
-
-    /**
-     * 设置监控任务ID
-     */
-    public void setMonitorJobId(String monitorJobId) {
-        this.monitorJobId = monitorJobId;
-    }
-
-    /**
-     * 获取监控成功次数
-     */
-    public int getMonitorSuccess() {
-        return monitorSuccess;
-    }
-
-    /**
-     * 设置监控成功次数
-     */
-    public void setMonitorSuccess(int monitorSuccess) {
-        this.monitorSuccess = monitorSuccess;
-    }
-
-    /**
-     * 获取监控失败次数
-     */
-    public int getMonitorFail() {
-        return monitorFail;
-    }
-
-    /**
-     * 设置监控失败次数
-     */
-    public void setMonitorFail(int monitorFail) {
-        this.monitorFail = monitorFail;
-    }
-
-    /**
-     * 获取监控状态
-     */
-    public int getMonitorStatus() {
-        return monitorStatus;
-    }
-
-    /**
-     * 设置监控状态
-     */
-    public void setMonitorStatus(int monitorStatus) {
-        this.monitorStatus = monitorStatus;
-    }
-
-    /**
-     * 获取运行状态
-     */
-    public String getRunStatus() {
-        return runStatus;
-    }
-
-    /**
-     * 设置运行状态
-     */
-    public void setRunStatus(String runStatus) {
-        this.runStatus = runStatus;
-    }
-
-    /**
-     * 获取上次执行时间
-     */
-    public Date getLastExecuteTime() {
-        return lastExecuteTime;
-    }
-
-    /**
-     * 设置上次执行时间
-     */
-    public void setLastExecuteTime(Date lastExecuteTime) {
-        this.lastExecuteTime = lastExecuteTime;
-    }
-
-    /**
-     * 获取下次执行时间
-     */
-    public Date getNextExecuteTime() {
-        return nextExecuteTime;
-    }
-
-    /**
-     * 设置下次执行时间
-     */
-    public void setNextExecuteTime(Date nextExecuteTime) {
-        this.nextExecuteTime = nextExecuteTime;
-    }
 }

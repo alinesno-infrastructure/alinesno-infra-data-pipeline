a959396d1f9311f7f1b8beeb047d9ce3f2032a3c
李伟(Michael) <michaelAip@gmail.com>
2024-08-18 01:23:00 -0400

    新增SinkReaderEnums枚举类的MYSQL元素，完善数据源类型枚举。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/enums/SinkReaderEnums.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/enums/SinkReaderEnums.java
@@ -1,15 +1,25 @@
 package com.alinesno.infra.data.pipeline.scheduler.enums;
 
+import lombok.Getter;
+
 /**
  * 数据源写入器枚举类
  * 用于表示不同的数据源类型
  */
+@Getter
 public enum SinkReaderEnums {
 
     CLICKHOUSE("clickhouse"), // ClickHouse数据源
     MINIO("minio"), // MinIO数据源
+    MYSQL("mysql"), // MYSQL数据源
     DORIS("doris"); // Doris数据源
 
+    /**
+     * -- GETTER --
+     *  获取数据源代码值
+     *
+     * @return 数据源代码值
+     */
     private final String code;
 
     /**
@@ -20,11 +30,4 @@ public enum SinkReaderEnums {
         this.code = code;
     }
 
-    /**
-     * 获取数据源代码值
-     * @return 数据源代码值
-     */
-    public String getCode() {
-        return code;
-    }
 }

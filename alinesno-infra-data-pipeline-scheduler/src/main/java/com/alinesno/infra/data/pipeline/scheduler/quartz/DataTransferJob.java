07261305302c969d2ea685ac91e9dab66cd1131a
陈伟(Victor) <victorAip@gmail.com>
2024-08-18 10:00:00 -0400

    新增数据转换任务类 DataTransferJob，实现定时创建数据调度作业功能。

---
+++ alinesno-infra-data-pipeline-scheduler/src/main/java/com/alinesno/infra/data/pipeline/scheduler/quartz/DataTransferJob.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.data.pipeline.scheduler.quartz;
+
+import com.alinesno.infra.common.core.context.SpringContext;
+import com.alinesno.infra.data.pipeline.scheduler.IDistSchedulerService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * 数据转换任务
+ */
+@Slf4j
+@PersistJobDataAfterExecution
+@DisallowConcurrentExecution
+public class DataTransferJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap() ;
+
+        Long jobId = jobDataMap.getLong("jobId") ;
+        log.debug("JobId = {}" , jobId);
+
+        IDistSchedulerService distSchedulerService = SpringContext.getBean(IDistSchedulerService.class) ;
+
+        try {
+            distSchedulerService.createCronJob(jobId);
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}

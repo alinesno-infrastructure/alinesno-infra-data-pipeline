407e5d333c21bbfaa89863fbab32dfbe8265e160
张婷(Tina) <tinaAip@gmail.com>
2024-08-22 00:56:00 -0400

    优化Scheduled类结构，使用Lombok注解简化代码。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/Scheduled.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/Scheduled.java
@@ -1,32 +1,16 @@
 package com.alinesno.infra.data.pipeline.scheduler;
 
+import lombok.Data;
+
 import java.time.Instant;
 
+@Data
 public class Scheduled {
+
     public final String taskName;
     public final String id;
     public final Instant executionTime;
     public final Object data;
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public Instant getExecutionTime() {
-        return executionTime;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public Scheduled() {
-      this(null, null, null, null);
-    }
 
     public Scheduled(String taskName, String id, Instant executionTime, Object data) {
       this.taskName = taskName;

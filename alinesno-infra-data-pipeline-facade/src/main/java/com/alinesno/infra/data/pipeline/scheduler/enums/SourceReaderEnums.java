57ad0530653133ad5055ddc3d73009e37a5cfc57
张婷(Tina) <tinaAip@gmail.com>
2024-08-22 01:44:00 -0400

    更新SourceReaderEnums枚举类，添加更多数据源类型及图标。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/enums/SourceReaderEnums.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/scheduler/enums/SourceReaderEnums.java
@@ -1,38 +1,72 @@
 package com.alinesno.infra.data.pipeline.scheduler.enums;
 
+import lombok.Getter;
+
 /**
  * 数据源读取器枚举类
  * 用于表示不同的数据源类型
  */
+@Getter
 public enum SourceReaderEnums {
 
-    KAFKA("kafka"), // Kafka数据源
-    HTTP("http"), // HTTP数据源
-    MYSQL("mysql"), // MySQL数据源
-    CLICKHOUSE("clickhouse"), // ClickHouse数据源
-    ORACLE("oracle"), // Oracle数据源
-    FTP("ftp"), // FTP数据源
-    ELASTICSEARCH("elasticsearch"), // Elasticsearch数据源
-    MINIO("minio"), // MinIO数据源
-    HIVE("hive"), // Hive数据源
-    REDIS("redis"), // Redis数据源
-    QINIU("qiniu"); // Qiniu数据源
+    CLICKHOUSE("clickhouse", "clickhouse.png"), // ClickHouse 数据源
+    MINIO("minio", "minio.png"), // MinIO 数据源
+    MYSQL("mysql", "mysql.png"), // MySQL 数据源
+    DORIS("doris", "doris.png"), // Doris 数据源
+    DB2("db2", "db2.png"), // DB2 数据源
+    GREENPLUM("greenplum", "greenplum.png"), // Greenplum 数据源
+    HIVE("hive", "hive.png"), // Hive 数据源
+    POSTGRESQL("postgresql", "postgresql.png"), // PostgreSQL 数据源
+    SQLSERVER("sqlserver", "sqlserver.png"), // SQL Server 数据源
+    ORACLE("oracle", "oracle.png"), // Oracle 数据源
+    REDIS("redis", "redis.png"), // Redis 数据源
+    MONGODB("mongodb", "mongodb.png"), // MongoDB 数据源
+    ELASTICSEARCH("elasticsearch", "elasticsearch.png"), // Elasticsearch 数据源
+    KAFKA("kafka", "kafka.png"), // Kafka 数据源
+    S3FILE("s3file", "s3file.png"), // S3 文件数据源
+    FTP("ftp", "ftp.png"), // FTP 数据源
+    SFTP("sftp", "sftp.png"), // SFTP 数据源
+    LOCALFILE("localfile", "localfile.png"), // 本地文件数据源
+    CSV("csv", "csv.png"), // CSV 数据源
+    EXCEL("excel", "excel.png"), // Excel 数据源
+    JSON("json", "json.png"), // JSON 数据源
+    XML("xml", "xml.png"), // XML 数据源
+    HTTP("http", "http.png"), // HTTP 数据源
+    HBASE("hbase", "hbase.png"), // HBase 数据源
+    HADOOP("hadoop", "hadoop.png"), // Hadoop 数据源
+    KUDU("kudu", "kudu.png"), // Kudu 数据源
+    QINIU("qiniu", "qiniu.png"), // Qiniu 数据源
+    STARROCKS("starrocks", "starrocks.png"); // StarRocks 数据源
 
+    /**
+     * -- GETTER --
+     *  获取数据源代码值
+     *
+     * @return 数据源代码值
+     */
     private final String code;
+    private final String icon ;
 
     /**
      * 构造函数
+     *
      * @param code 数据源代码值
+     * @param icon
      */
-    SourceReaderEnums(String code) {
+    SourceReaderEnums(String code, String icon) {
         this.code = code;
+        this.icon = icon;
     }
 
     /**
-     * 获取数据源代码值
-     * @return 数据源代码值
+     * 获取到所有数据源类型List列表
      */
-    public String getCode() {
-        return code;
+    public static String[] getAllSourceReader() {
+        SourceReaderEnums[] values = SourceReaderEnums.values();
+        String[] sourceReader = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            sourceReader[i] = values[i].getCode();
+        }
+        return sourceReader;
     }
 }

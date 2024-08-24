6ce7278ef5e4ecf024f2624accd290d8607e443d
李伟(Michael) <michaelAip@gmail.com>
2024-08-24 00:31:00 -0400

    优化了服务视图组件的布局和内容展示，调整了数据集成统计部分的显示方式。

--- alinesno-infra-data-pipeline-ui/src/views/operation-workspace/service.vue
+++ alinesno-infra-data-pipeline-ui/src/views/operation-workspace/service.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row class="acp-dashboard-panel" :gutter="20">
 
-      <el-col :span="19">
+      <el-col :span="18">
 
         <div class="grid-content">
           <div class="panel-header">
@@ -26,20 +26,24 @@
 
       </el-col>
 
-      <el-col :span="5">
+      <el-col :span="6">
         <div class="grid-content">
           <div class="panel-header">
-            <div class="header-title"><i class="fa-solid fa-user-nurse"></i> 数据集成统计</div>
+            <div class="header-title"><i class="fa-solid fa-user-nurse"></i> 运行情况统计</div>
           </div>
           <div class="panel-body acp-height-auto">
-            <ul class="panel-item-text">
+                <div
+                  id="echarts-bar3-chart"
+                  :style="{ width: '100%', height: '250px' }"
+                ></div>
+            <!-- <ul class="panel-item-text">
               <li style="width:calc(50% - 20px);margin:10px;padding:10px;" v-for="item in opertionAssets" :key="item.id">
                 <div class="item-health-box">
                   <div class="item-health-title">{{ item.title }}</div>
                   <div class="item-health-count">{{ item.count }}</div>
                 </div>
               </li>
-            </ul>
+            </ul> -->
           </div>
         </div>
       </el-col>
@@ -64,97 +68,139 @@ const opertionAssets = ref([
 ])
 
 function drawBar2() {
-      let barChart = echart.init(
-        document.getElementById("echarts-bar2-chart")
-      );
+  let barChart = echart.init(
+    document.getElementById("echarts-bar2-chart")
+  );
 
-      var lineOption;
+  var lineOption;
 
-      // prettier-ignore
-      const hours = [
-          '12a', '1a', '2a', '3a', '4a', '5a', '6a',
-          '7a', '8a', '9a', '10a', '11a',
-          '12p', '1p', '2p', '3p', '4p', '5p',
-          '6p', '7p', '8p', '9p', '10p', '11p'
-      ];
-      // prettier-ignore
-      const days = ['星期六', '星期五', '星期四','星期三', '星期二', '星期一', '星期日'];
+  // prettier-ignore
+  const hours = [
+      '12a', '1a', '2a', '3a', '4a', '5a', '6a',
+      '7a', '8a', '9a', '10a', '11a',
+      '12p', '1p', '2p', '3p', '4p', '5p',
+      '6p', '7p', '8p', '9p', '10p', '11p'
+  ];
+  // prettier-ignore
+  const days = ['星期六', '星期五', '星期四','星期三', '星期二', '星期一', '星期日'];
 
-      // prettier-ignore
-      const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0],
-                    [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2],
-                    [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0],
-                    [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0],
-                    [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4],
-                    [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5],
-                    [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3],
-                    [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6],
-                    [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5],
-                    [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8],
-                    [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4],
-                    [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0],
-                    [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2],
-                    [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8],
-                    [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1],
-                    [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]]
-          .map(function (item) {
-          return [item[1], item[0], item[2] || '-'];
-      });
+  // prettier-ignore
+  const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0],
+                [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2],
+                [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0],
+                [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0],
+                [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4],
+                [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5],
+                [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3],
+                [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6],
+                [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5],
+                [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8],
+                [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4],
+                [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0],
+                [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2],
+                [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8],
+                [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1],
+                [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]]
+      .map(function (item) {
+      return [item[1], item[0], item[2] || '-'];
+  });
 
-      lineOption= {
-        tooltip: {
-          position: 'top'
+  lineOption= {
+    tooltip: {
+      position: 'top'
+    },
+    grid: {
+      height: '90%',
+      width: '90%',
+      top: '1%'
+    },
+    xAxis: {
+      type: 'category',
+      data: hours,
+      splitArea: {
+        show: true
+      }
+    },
+    yAxis: {
+      type: 'category',
+      data: days,
+      splitArea: {
+        show: true
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 10,
+      calculable: true,
+      orient: '',
+      left: 'left',
+      bottom: '0%'
+    },
+    series: [
+      {
+        name: '异常次数',
+        type: 'heatmap',
+        data: data,
+        label: {
+          show: true
         },
-        grid: {
-          height: '90%',
-          width: '90%',
-          top: '1%'
-        },
-        xAxis: {
-          type: 'category',
-          data: hours,
-          splitArea: {
-            show: true
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
           }
-        },
-        yAxis: {
-          type: 'category',
-          data: days,
-          splitArea: {
-            show: true
-          }
-        },
-        visualMap: {
-          min: 0,
-          max: 10,
-          calculable: true,
-          orient: '',
-          left: 'left',
-          bottom: '0%'
-        },
-        series: [
-          {
-            name: '异常次数',
-            type: 'heatmap',
-            data: data,
-            label: {
-              show: true
-            },
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            }
-          }
-        ]
-      };
+        }
+      }
+    ]
+  };
 
-      barChart.setOption(lineOption);
-    } ;
+  barChart.setOption(lineOption);
+} ;
+
+function drawBar3() {
+
+  let barChart = echart.init(
+    document.getElementById("echarts-bar3-chart")
+  );
+
+  let option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: '任务数量',
+        type: 'pie',
+        radius: '85%',
+        data: [
+          {value: 335, name: '已完成'},
+          {value: 310, name: '执行中'},
+          {value: 234, name: '异常任务'},
+          {value: 135, name: '成功'},
+          {value: 154, name: '失败'},
+          {value: 250, name: '运行中'}
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  barChart.setOption(option);
+}
 
 onMounted(() => {
-  drawBar2() ;
+  drawBar2();
+  drawBar3();
 });
 
 onUnmounted(() => {

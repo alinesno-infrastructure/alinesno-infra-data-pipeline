0b692f2a6e50b889069b6ce76988d6b4e34bfffd
李伟(Michael) <michaelAip@gmail.com>
2024-08-24 08:53:00 -0400

    修复并优化数据采集与写入源选择界面

--- alinesno-infra-data-pipeline-ui/src/views/data/pipeline/readerSource/addSource.vue
+++ alinesno-infra-data-pipeline-ui/src/views/data/pipeline/readerSource/addSource.vue
@@ -7,76 +7,134 @@
       <div class="sub-tip">根据业务场景需求创建数据采集任务，便于数据业务场景开发和分析</div>
     </div>
     <div class="form-container" >
-         <el-form ref="databaseRef" :model="form" :rules="rules" label-width="100px">
-
-            <el-form-item label="源类型" prop="readerType">
-               <el-radio-group v-model="form.readerType">
-                  <el-radio key="R" label="R" value="R"  size="large">读取(reader)</el-radio>
-                  <el-radio key="S" label="S" value="S"  size="large">写入(sink)</el-radio>
-               </el-radio-group> 
-            </el-form-item>
-
-            <!-- 数据采集模板 -->
-            <el-form-item label="数据采集源" prop="readerSource" v-if="form.readerType == 'R'">
-               <el-radio-group v-model="form.readerSource">
-                  <el-radio v-for="item in readerSource" 
-                     class="database-type" 
-                     :key="item.id"
-                     :label="item.id"
-                     :value="item.id" 
-                     size="large">
-                     <div style="float: left;">
-                        <img style="width:30px; height:30px" :src="'http://data.linesno.com/icons/database/' + item.readerType + '.png'" /> 
-                     </div>
-                     <div style="float: left;margin-left: 10px;line-height: 1.2rem;">
-                           {{ item.readerName }}
-                           <div style="font-size: 11px;">
-                           {{ item.readerDesc }} 
-                        </div>
-                     </div>
-                  </el-radio>
-               </el-radio-group> 
-
-            </el-form-item>
-
-            <el-form-item label="数据写入源" prop="sinkSource" v-if="form.readerType == 'S'">
-               <el-radio-group v-model="form.sinkSource">
-                  <el-radio v-for="item in sinkSource" 
-                     class="database-type" 
-                     :key="item.id"
-                     :label="item.id"
-                     :value="item.id" 
-                     size="large">
-                     <div style="float: left;">
-                        <img style="width:30px; height:30px" :src="'http://data.linesno.com/icons/database/' + item.readerType + '.png'" /> 
-                     </div>
-                     <div style="float: left;margin-left: 10px;line-height: 1.2rem;">
-                           {{ item.readerName }}
-                           <div style="font-size: 11px;">
-                           {{ item.readerDesc }} 
-                        </div>
-                     </div>
-                  </el-radio>
-               </el-radio-group> 
-
-            </el-form-item>
-
+         <el-form ref="databaseRef" :model="form" :rules="rules" label-width="120px">
 
             <el-form-item label="描述" prop="readerDesc">
                <el-input v-model="form.readerDesc" placeholder="读取源描述"/>
             </el-form-item>
 
-            <el-form-item label="连接" prop="readerUrl">
-               <el-input v-model="form.readerUrl" placeholder="jdbc:mysql://localhost:3306/readerName?useUnicode=true&characterEncoding=utf8&characterSetResults=utf8&useSSL=false&serverTimezone=GMT"/>
+            <el-form-item label="源组织" prop="owner">
+               <el-input v-model="form.owner" style="width:50%" placeholder="读取源组织"/>
             </el-form-item>
 
-            <el-form-item label="用户名" prop="readerUsername">
-               <el-input v-model="form.readerUsername" auto-complete="new-password" placeholder="读取源用户名"/>
+            <el-form-item label="源类型" prop="operationType">
+               <el-radio-group v-model="form.operationType" @change="changeReaderType()">
+                  <el-radio key="source" label="source" value="source"  size="large">读取(reader)</el-radio>
+                  <el-radio key="sink" label="sink" value="sink"  size="large">写入(sink)</el-radio>
+               </el-radio-group> 
             </el-form-item>
 
-            <el-form-item label="密码" prop="readerPasswd">
-               <el-input type="password" auto-complete="new-password" v-model="form.readerPasswd" placeholder="读取源密码"/>
+            <!-- 数据采集模板 -->
+            <el-form-item label="数据采集源" prop="readerType">
+               <el-radio-group v-model="form.readerType">
+                  <el-radio v-for="item in form.operationType=='source'?readerType:sinkSource" 
+                     class="database-type" 
+                     :key="item.code"
+                     :label="item.code"
+                     :value="item.code" 
+                     size="large">
+                     <div style="float: left;">
+                        <img style="width:30px; height:30px" :src="'http://data.linesno.com/icons/database/' + item.icon+ ''" /> 
+                     </div>
+                     <div style="float: left;margin-left: 10px;line-height: 1.2rem;">
+                           {{ item.code}}
+                           <div style="font-size: 11px;">
+                           {{ item.readerDesc }} 
+                        </div>
+                     </div>
+                  </el-radio>
+               </el-radio-group> 
+
             </el-form-item>
+
+               <!-- 数据源配置项 -->
+               <div v-if="
+                        form.readerType === 'mysql' || 
+                        form.readerType === 'doris' || 
+                        form.readerType === 'clickhouse' || 
+                        form.readerType === 'postgresql' ||
+                        form.readerType === 'sqlserver' ||
+                        form.readerType === 'hive' ||
+                        form.readerType === 'oracle'
+                        ">
+
+                  <el-form-item label="连接" prop="readerUrl">
+                     <el-input v-model="form.readerUrl" placeholder="jdbc:mysql://localhost:3306/readerName?useUnicode=true&characterEncoding=utf8&characterSetResults=utf8&useSSL=false&serverTimezone=GMT"/>
+                  </el-form-item>
+
+                  <el-form-item label="用户名" prop="readerUsername">
+                     <el-input v-model="form.readerUsername" style="width:50%"  auto-complete="new-password" placeholder="读取源用户名"/>
+                  </el-form-item>
+
+                  <el-form-item label="密码" prop="readerPasswd">
+                     <el-input type="password" auto-complete="new-password" :show-password="true" style="width:50%"  v-model="form.readerPasswd" placeholder="读取源密码"/>
+                  </el-form-item>
+               </div>
+
+               <!-- SSO配置项 -->
+               <div v-if="
+                        form.readerType === 'minio' || 
+                        form.readerType === 'qiniu' || 
+                        form.readerType === 's3file'
+                        ">
+                  <el-form-item label="地址" prop="readerUrl">
+                     <el-input v-model="form.readerUrl" placeholder="SSO文件读取地址"/>
+                  </el-form-item>
+                  <el-form-item label="AccessKey" prop="accessKey">
+                     <el-input v-model="form.accessKey" style="width:50%" :show-password="true" placeholder="请求密钥"/>
+                  </el-form-item>
+                  <el-form-item label="SecretKey" prop="secretKey">
+                     <el-input v-model="form.secretKey" style="width:50%" :show-password="true" placeholder="请求Security"/>
+                  </el-form-item>
+               </div>
+
+               <!-- Ftp/ElasticSearch/Redis 配置项 -->
+               <div v-if="
+                        form.readerType === 'elasticsearch'  ||
+                        form.readerType === 'ftp'  ||
+                        form.readerType === 'sftp'  ||
+                        form.readerType === 'redis' 
+                        ">
+                  <el-form-item label="Host地址" prop="readerUrl">
+                     <el-input v-model="form.readerUrl" placeholder="ElasticSearch地址"/>
+                  </el-form-item>
+                  <el-form-item label="用户名" prop="readerUsername" 
+                     v-if="
+                        form.readerType !== 'redis' 
+                     ">
+                     <el-input v-model="form.readerUsername" style="width:50%" placeholder="请求用户名"/>
+                  </el-form-item>
+                  <el-form-item label="密码" prop="readerUsername">
+                     <el-input v-model="form.readerPasswd" style="width:50%" :show-password="true" placeholder="请求密码"/>
+                  </el-form-item>
+               </div>
+
+               <!-- Kafka配置项/ActiveMQ配置项 -->
+               <div v-if="
+                        form.readerType === 'kafka'  ||
+                        form.readerType === 'activemq' 
+                        ">
+                  <el-form-item label="Server地址" prop="readerUrl">
+                     <el-input v-model="form.readerUrl" placeholder="Server地址"/>
+                  </el-form-item>
+               </div>
+
+               <!-- cvs/excel配置项 -->
+               <div v-if="
+                        form.readerType === 'csv'  ||
+                        form.readerType === 'localfile'  ||
+                        form.readerType === 'excel' 
+                        ">
+                  <el-form-item label="文件地址" prop="readerUrl">
+                     <el-input v-model="form.readerUrl" placeholder="文件地址"/>
+                  </el-form-item>
+                  <el-form-item label="用户名" prop="readerUsername">
+                     <el-input v-model="form.readerUsername" style="width:50%" placeholder="地址认证用户名"/>
+                  </el-form-item>
+                  <el-form-item label="密码" prop="readerPasswd">
+                     <el-input v-model="form.readerPasswd" style="width:50%" :show-password="true" placeholder="地址认证密码"/>
+                  </el-form-item>
+               </div>
 
             </el-form>
 
@@ -84,8 +142,8 @@
                <!-- <el-button type="primary" @click="submitForm">确 定</el-button>
                <el-button @click="cancel">取 消</el-button> -->
                <el-button type="text" @click="validateDburl">请先点击验证读取源是否连通</el-button>
-               <el-button type="primary" @click="submitForm" :disabled="!btnChangeEnable">确 定</el-button>
-               <el-button @click="cancel">取 消</el-button>
+               <el-button type="primary" @click="submitForm" :disabled="!btnChangeEnable">确认保存</el-button>
+               <!-- <el-button @click="cancel">取 消</el-button> -->
             </div>
       </div>
    </div>
@@ -98,36 +156,26 @@ import {
    delReaderSource,
    getReaderSource,
    checkDbConfig,
+   checkConnectionByObj,
    updateReaderSource,
-   addReaderSource
+   addReaderSource ,
+   allReaderSource
 } from "@/api/data/pipeline/readerSource";
 
-import {
-  getAllSourceReader,
-} from "@/api/data/pipeline/jobBuilder";
+import { ElLoading } from 'element-plus'
+import { nextTick } from "vue";
 
 const router = useRouter();
+const route = useRoute();
 const { proxy } = getCurrentInstance();
 
 // 定义变量
-const ReaderSourceList = ref([]);
 const open = ref(false);
 const loading = ref(true);
-const showSearch = ref(true);
-const ids = ref([]);
-const readerDescs = ref([]);
-const single = ref(true);
-const multiple = ref(true);
-const total = ref(0);
-const title = ref("");
-const dateRange = ref([]);
 const btnChangeEnable = ref(true)
 
-const readerSource = ref([])  // 读取源
+const readerType = ref([])  // 读取源
 const sinkSource = ref([])  // 数据目的
-
-const postOptions = ref([]);
-const roleOptions = ref([]);
 
 // 列显隐信息
 const columns = ref([
@@ -142,49 +190,58 @@ const columns = ref([
 
 const data = reactive({
    form: {
-      readerType: 'R'
-   },
-   queryParams: {
-      pageNum: 1,
-      pageSize: 10,
-      readerName: undefined,
-      readerDesc: undefined
+      operationType: 'source'
    },
    rules: {
       readerName: [{ required: true, message: "名称不能为空", trigger: "blur" }] , 
+      owner: [{ required: true, message: "源组织不能为空", trigger: "blur" }] , 
+      readerType: [{ required: true, message: "数据源类型不能为空", trigger: "blur" }] , 
       readerUrl: [{ required: true, message: "连接不能为空", trigger: "blur" }],
-      readerType: [{ required: true, message: "类型不能为空", trigger: "blur" }] , 
+      operationType: [{ required: true, message: "类型不能为空", trigger: "blur" }] , 
       readerUsername: [{ required: true , message: "用户名不能为空", trigger: "blur"}],
       readerPasswd: [{ required: true, message: "密码不能为空", trigger: "blur" }] , 
+      accessKey: [{ required: true, message: "密钥不能为空", trigger: "blur" }] , 
+      secretKey: [{ required: true, message: "密钥不能为空", trigger: "blur" }] , 
       readerDesc: [{ required: true, message: "备注不能为空", trigger: "blur" }] 
    }
 });
 
-const { queryParams, form, rules } = toRefs(data);
+const { form, rules } = toRefs(data);
 
-/** 查询应用列表 */
-function getList() {
-   loading.value = true;
-   listReaderSource(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
-      loading.value = false;
-      ReaderSourceList.value = res.rows;
-      total.value = res.total;
-   });
-};
+/** 修改状态信息 */
+function changeReaderType(val){
+   form.value.readerType = '' ;
+}
 
 /** 获取到所有数据源 */
 function handleGetAllSourceReader() {
-  getAllSourceReader().then(response => {
+   allReaderSource().then(response => {
     const data = response.data;
 
-    readerSource.value = data.filter(item => item.sourceType === 'sink');
+    readerType.value = data.filter(item => item.sourceType === 'sink');
     sinkSource.value = data.filter(item => item.sourceType === 'source');
 
-    readerSource.value = data ; 
-    sinkSource.value = data ; 
+   //  readerType.value = data ; 
+   //  sinkSource.value = data ; 
 
     console.log('sinkSource = ' + sinkSource.value)
+
+    nextTick(() => {
+      handleSourceInfo();
+    });
   });
+}
+
+/** 通过id获取到源信息 */
+function handleSourceInfo(){
+
+   let id =route.query.id;
+
+   if(id){
+      getReaderSource(id).then(response => {
+         form.value = response.data;
+      });
+   }
 }
 
 /** 返回 */
@@ -196,17 +253,16 @@ function goBack() {
 function validateDburl() {
    proxy.$refs["databaseRef"].validate(valid => {
       if (valid) {
-         if(!form.readerType){
-            form.value.readerType = 'MySQL' ;
-         }
-
-         checkDbConfig(form.value).then(resp=>{
-            if (resp.data.accepted){
-               proxy.$modal.msgSuccess("读取源校验成功");
-               btnChangeEnable.value = true ;
-            }else{
-               proxy.$modal.msgSuccess(resp.msg);
-            }
+         const loading = ElLoading.service({
+            lock: true,
+            text: form.value.readerDesc + '连接验证中',
+            background: 'rgba(0, 0, 0, 0.7)',
+         })
+         checkConnectionByObj(form.value).then(resp=>{
+            proxy.$modal.msgSuccess("读取源校验成功");
+            loading.close()
+         }).catch(error => {
+            loading.close()
          })
 
       }

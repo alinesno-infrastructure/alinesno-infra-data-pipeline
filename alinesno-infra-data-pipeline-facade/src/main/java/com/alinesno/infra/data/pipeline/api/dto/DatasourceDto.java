4ae9900e8189d5f392b9149eaf2588e396640889
李伟(Michael) <michaelAip@gmail.com>
2024-08-24 03:51:00 -0400

    优化 DatasourceDto 类属性注释与规则

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/api/dto/DatasourceDto.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/api/dto/DatasourceDto.java
@@ -13,22 +13,38 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class DatasourceDto extends BaseDto {
-	
-	@NotBlank(message = "数据库描述")
-	private String dbDesc;
-	
-	private String dbName;
-	
-	@NotBlank(message = "数据库用户名为空")
-	private String jdbcUrl ;
-	
-	@NotBlank(message = "数据库用户名为空")
-	private String dbUsername;
-	
-	@NotBlank(message = "数据库密码为空")
-	private String dbPasswd;
-	
-	@NotBlank(message = "数据库类型为空")
-	private String dbType;
+
+	@NotBlank(message = "描述不能为空")
+	private String readerDesc;
+
+//	@NotBlank(message = "名称不能为空")
+	private String readerName;
+
+	@NotBlank(message = "连接不能为空")
+	private String readerUrl;
+
+//	@ConditionalNotBlank(conditionField = "readerType", conditionValue = "mysql|oracle", message = "用户名不能为空")
+	private String readerUsername;
+
+//	@ConditionalNotBlank(conditionField = "readerType", conditionValue = "mysql|oracle", message = "密码不能为空")
+	private String readerPasswd;
+
+//	@NotBlank(message = "accessKey不能为空")
+	private String accessKey ;
+
+//	@NotBlank(message = "secretKey不能为空")
+	private String secretKey ;
+
+//	@NotBlank(message = "端口不能为空")
+	private String readerPort;
+
+	@NotBlank(message = "类型不能为空")
+	private String readerType;
+
+	@NotBlank(message = "数据来源组织不能为空")
+	private String owner;
+
+	@NotBlank(message = "操作类型不能为空")
+	private String operationType;
 
 }

f68515fe33ae695fcb44dccaaa4e0974bee49384
李娜(Allen) <allenAip@gmail.com>
2024-08-26 18:46:00 -0400

    新增转换Service接口ITransformService。

---
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/service/ITransformService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.data.pipeline.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
+
+/**
+ * 转换Service接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface ITransformService extends IBaseService<TransformEntity> {
+}

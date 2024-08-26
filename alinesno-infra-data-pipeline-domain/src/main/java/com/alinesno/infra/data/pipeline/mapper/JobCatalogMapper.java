07cdf6ba34da116e90aee012bb7d652da8f45740
李娜(Allen) <allenAip@gmail.com>
2024-08-26 18:37:00 -0400

    新增JobCatalogMapper接口文件。

---
+++ alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/mapper/JobCatalogMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.data.pipeline.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.pipeline.entity.JobCatalogEntity;
+
+/**
+ * Prompt指令类型Mapper接口
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface JobCatalogMapper extends IBaseMapper<JobCatalogEntity>{
+}

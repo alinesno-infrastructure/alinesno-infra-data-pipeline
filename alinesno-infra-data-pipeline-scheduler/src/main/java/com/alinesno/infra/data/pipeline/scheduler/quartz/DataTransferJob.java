0d2f848eba8d573c1df131f06eb7e4e98dd42a8f
李娜(Allen) <allenAip@gmail.com>
2024-08-26 05:22:00 -0400

    优化 DataTransferJob 类，引入 JobInstance 服务及雪花算法生成任务实例ID，处理执行状态与异常。

--- alinesno-infra-data-pipeline-scheduler/src/main/java/com/alinesno/infra/data/pipeline/scheduler/quartz/DataTransferJob.java
+++ alinesno-infra-data-pipeline-scheduler/src/main/java/com/alinesno/infra/data/pipeline/scheduler/quartz/DataTransferJob.java
@@ -1,23 +1,27 @@
 package com.alinesno.infra.data.pipeline.scheduler.quartz;
 
+import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.core.context.SpringContext;
+import com.alinesno.infra.data.pipeline.enums.JobStatusEnums;
 import com.alinesno.infra.data.pipeline.scheduler.IQuartzSchedulerService;
+import com.alinesno.infra.data.pipeline.service.IJobInstanceService;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
-
-import java.io.IOException;
-import java.sql.SQLException;
 
 /**
  * 数据转换任务
  */
 @Slf4j
-@PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 public class DataTransferJob implements Job {
 
+    private IJobInstanceService jobInstanceService ;
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        jobInstanceService = SpringContext.getBean(IJobInstanceService.class) ;
+        long jobInstanceId = IdUtil.getSnowflakeNextId() ;
 
         JobDataMap jobDataMap = context.getJobDetail().getJobDataMap() ;
 
@@ -26,11 +30,24 @@ public class DataTransferJob implements Job {
 
         IQuartzSchedulerService distSchedulerService = SpringContext.getBean(IQuartzSchedulerService.class) ;
 
+        String status = JobStatusEnums.COMPLETED.getStatus();
+        String message = null ;
         try {
+
+            jobInstanceService.startMonitorJob(jobId , jobInstanceId) ;
             distSchedulerService.createCronJob(jobId);
-        } catch (SQLException | IOException e) {
+
+            jobInstanceService.finishMonitorJob(jobId , jobInstanceId , status , message) ;
+        } catch (Exception e) {
+
+            log.error("jobId = {} , error = {}", jobId, e.getMessage());
+            status = JobStatusEnums.FAILED.getStatus();
+            message = e.getMessage() ;
+
+            jobInstanceService.finishMonitorJob(jobId , jobInstanceId , status , message) ;
             throw new RuntimeException(e);
         }
+
 
     }
 

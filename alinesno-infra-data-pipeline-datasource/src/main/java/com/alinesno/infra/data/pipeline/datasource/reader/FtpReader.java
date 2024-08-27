6a773425e6faa212a7fdd2d2dd69a20b3de4676c
李娜(Allen) <allenAip@gmail.com>
2024-08-27 03:41:00 -0400

    修复FTP读取组件实现，更新readData方法至适用于TransformEntity。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/reader/FtpReader.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/reader/FtpReader.java
@@ -2,21 +2,17 @@ package com.alinesno.infra.data.pipeline.datasource.reader;
 
 import com.alinesno.infra.data.pipeline.constants.PipeConstants;
 import com.alinesno.infra.data.pipeline.datasource.ComponentSourceReader;
-import com.alinesno.infra.data.pipeline.datasource.IDataSourceReader;
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
 import com.alinesno.infra.data.pipeline.scheduler.dto.TaskInfoDto;
-import com.alinesno.infra.data.pipeline.transfer.bean.FieldMetaBean;
-import com.alinesno.infra.data.pipeline.transfer.bean.ReaderSourceBean;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.sql.SQLException;
-import java.util.List;
 
 @Component("ftp" + PipeConstants.READER_SUFFIX)
 public class FtpReader  extends ComponentSourceReader {
     @Override
-    public File readData(TaskInfoDto taskInfoDto, String jobWorkspace, TransEntity trans) throws SQLException {
+    public File readData(TaskInfoDto taskInfoDto, String jobWorkspace, TransformEntity trans) throws SQLException {
         return null;
     }
 

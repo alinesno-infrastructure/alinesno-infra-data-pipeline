cdc26958be264038842016882fb73fc6a94b98cb
李娜(Allen) <allenAip@gmail.com>
2024-08-27 04:41:00 -0400

    优化 TransEvent 类结构，添加 getter 和 setter 方法及 Lombok 注解。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/event/TransEvent.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/event/TransEvent.java
@@ -1,51 +1,26 @@
 package com.alinesno.infra.data.pipeline.datasource.event;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.context.ApplicationEvent;
 
 /**
  * TransEvent 事件
  */
+@ToString
+@Setter
+@Getter
 public class TransEvent extends ApplicationEvent {
 
-    private Long totalCount ;
-    private Long transCount;
-    private Long transId ;
+    private long totalCount ;
+    private long transCount;
+    private long transId ;
+    private long jobInstanceId;
 
     public TransEvent(Long transId) {
         super(transId);
         this.transId = transId ;
     }
 
-    public Long getTransId() {
-        return transId;
-    }
-
-    public void setTransId(Long transId) {
-        this.transId = transId;
-    }
-
-    public Long getTotalCount() {
-        return totalCount;
-    }
-
-    public void setTotalCount(Long totalCount) {
-        this.totalCount = totalCount;
-    }
-
-    public Long getTransCount() {
-        return transCount;
-    }
-
-    public void setTransCount(Long transCount) {
-        this.transCount = transCount;
-    }
-
-    @Override
-    public String toString() {
-        return "TransEvent{" +
-                "totalCount=" + totalCount +
-                ", transCount=" + transCount +
-                ", transId=" + transId +
-                '}';
-    }
 }

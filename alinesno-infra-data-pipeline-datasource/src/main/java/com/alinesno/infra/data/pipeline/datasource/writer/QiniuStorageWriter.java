83a0087e16e2f81a6cd731a558f1641b153f912b
李娜(Allen) <allenAip@gmail.com>
2024-08-27 03:18:00 -0400

    fix: 更新 QiniuStorageWriter 类，将 TransEntity 更改为 TransformEntity。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/QiniuStorageWriter.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/QiniuStorageWriter.java
@@ -2,7 +2,7 @@ package com.alinesno.infra.data.pipeline.datasource.writer;
 
 import com.alinesno.infra.data.pipeline.constants.PipeConstants;
 import com.alinesno.infra.data.pipeline.datasource.ComponentSinkWriter;
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
 import com.alinesno.infra.data.pipeline.scheduler.dto.SinkWriter;
 import com.alinesno.infra.data.pipeline.scheduler.dto.TaskInfoDto;
 import com.qiniu.storage.Configuration;
@@ -35,7 +35,7 @@ public class QiniuStorageWriter extends ComponentSinkWriter {
      * @throws SQLException SQL异常
      */
     @Override
-    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransEntity trans) throws IOException, SQLException {
+    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransformEntity trans) throws IOException, SQLException {
 
         SinkWriter writer = taskInfoDto.getWriter();
 

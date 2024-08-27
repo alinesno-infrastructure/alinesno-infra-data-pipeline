ce361ace5c929543a77ae646518913346e855ba9
李娜(Allen) <allenAip@gmail.com>
2024-08-27 04:12:00 -0400

    fix: 更新AliyunStorageWriter类，替换TransEntity为TransformEntity。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/AliyunStorageWriter.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/AliyunStorageWriter.java
@@ -2,7 +2,7 @@ package com.alinesno.infra.data.pipeline.datasource.writer;
 
 import com.alinesno.infra.data.pipeline.constants.PipeConstants;
 import com.alinesno.infra.data.pipeline.datasource.ComponentSinkWriter;
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
 import com.alinesno.infra.data.pipeline.scheduler.dto.SinkWriter;
 import com.alinesno.infra.data.pipeline.scheduler.dto.TaskInfoDto;
 import com.aliyun.oss.OSS;
@@ -33,7 +33,7 @@ public class AliyunStorageWriter extends ComponentSinkWriter {
      * @throws SQLException SQL异常
      */
     @Override
-    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransEntity trans) throws IOException, SQLException {
+    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransformEntity trans) throws IOException, SQLException {
         SinkWriter writer = taskInfoDto.getWriter();
 
         String accessKey = writer.getAccessKey();

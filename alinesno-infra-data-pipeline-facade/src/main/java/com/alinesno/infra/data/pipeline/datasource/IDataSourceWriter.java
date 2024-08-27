b4364b567c8f733401c341368636c3cb6eed70a2
李娜(Allen) <allenAip@gmail.com>
2024-08-26 20:00:00 -0400

    fix:调整数据写入接口参数，从TransEntity更改为TransformEntity。

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/datasource/IDataSourceWriter.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/datasource/IDataSourceWriter.java
@@ -1,22 +1,17 @@
 package com.alinesno.infra.data.pipeline.datasource;
 
-import com.alibaba.druid.pool.DruidDataSourceFactory;
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
-import com.alinesno.infra.data.pipeline.scheduler.dto.SinkWriter;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
 import com.alinesno.infra.data.pipeline.scheduler.dto.TaskInfoDto;
 
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 数据写入
  */
 public interface IDataSourceWriter {
 
-    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransEntity trans) throws IOException, SQLException;
+    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransformEntity trans) throws IOException, SQLException;
 
 }

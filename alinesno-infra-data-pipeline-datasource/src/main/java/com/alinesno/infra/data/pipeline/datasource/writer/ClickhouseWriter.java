1867840e1fe37ec2e93d7b4129d59ae40173211e
李娜(Allen) <allenAip@gmail.com>
2024-08-26 20:31:00 -0400

    fix: 更新 ClickhouseWriter.java 文件，将 TransEntity 类替换为 TransformEntity 类。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/ClickhouseWriter.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/writer/ClickhouseWriter.java
@@ -7,7 +7,7 @@ import com.alinesno.infra.data.pipeline.datasource.MappingBean;
 import com.alinesno.infra.data.pipeline.datasource.event.TransEvent;
 import com.alinesno.infra.data.pipeline.datasource.event.TransEventPublisher;
 import com.alinesno.infra.data.pipeline.datasource.mapping.SQLFieldMapping;
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
 import com.alinesno.infra.data.pipeline.scheduler.dto.TaskInfoDto;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
@@ -48,7 +48,7 @@ public class ClickhouseWriter extends ComponentSinkWriter {
      * @throws SQLException SQL异常
      */
     @Override
-    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransEntity trans) throws IOException, SQLException {
+    public void writerData(TaskInfoDto taskInfoDto, File filterFile, TransformEntity trans) throws IOException, SQLException {
 
         List<JSONObject> jsonObjectList = new ArrayList<>();
         long count = 0L;

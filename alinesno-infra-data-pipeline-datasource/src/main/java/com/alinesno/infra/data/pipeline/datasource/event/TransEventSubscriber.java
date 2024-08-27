72558ae5efbe5e071fd0c970f3cbfbfadbfeb5a0
李娜(Allen) <allenAip@gmail.com>
2024-08-27 07:17:00 -0400

    优化事件订阅处理逻辑，将数据源实体类及服务接口更新为更具描述性的名称，并引入日志记录。

--- alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/event/TransEventSubscriber.java
+++ alinesno-infra-data-pipeline-datasource/src/main/java/com/alinesno/infra/data/pipeline/datasource/event/TransEventSubscriber.java
@@ -1,30 +1,30 @@
 package com.alinesno.infra.data.pipeline.datasource.event;
 
-import com.alinesno.infra.data.pipeline.entity.TransEntity;
-import com.alinesno.infra.data.pipeline.service.ITransService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.alinesno.infra.data.pipeline.entity.TransformEntity;
+import com.alinesno.infra.data.pipeline.entity.TransformMonitorEntity;
+import com.alinesno.infra.data.pipeline.service.IJobInstanceService;
+import com.alinesno.infra.data.pipeline.service.ITransformMonitorService;
+import com.alinesno.infra.data.pipeline.service.ITransformService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class TransEventSubscriber implements ApplicationListener<TransEvent> {
 
-    private static final Logger log = LoggerFactory.getLogger(TransEventSubscriber.class) ;
-
     @Autowired
-    private ITransService transService ;
+    private ITransformMonitorService transformMonitorService;
 
     @Override
     public void onApplicationEvent(TransEvent event) {
-        log.info("trans count = {}" , event);
+        log.info("--->>> 数据读取信息:trans count = {}" , event);
 
-        TransEntity trans = transService.getById(event.getTransId()) ;
+        TransformMonitorEntity trans = transformMonitorService.getById(event.getTransId()) ;
         trans.setTotalDataCount(event.getTotalCount());
         trans.setProcessDataCount(event.getTransCount());
 
-        transService.update(trans) ;
+        transformMonitorService.update(trans) ;
     }
 }

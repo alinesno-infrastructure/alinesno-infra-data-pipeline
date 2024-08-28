c57201e65864db2bf54cfd415af9b902e9c14c4e
周健(James) <jamesAip@gmail.com>
2024-08-27 20:23:00 -0400

    优化表格展示逻辑，添加当前行数据获取方法。

--- alinesno-infra-data-pipeline-ui/src/views/data/pipeline/task/params/dsFetchTableData.vue
+++ alinesno-infra-data-pipeline-ui/src/views/data/pipeline/task/params/dsFetchTableData.vue
@@ -9,10 +9,10 @@
         style="width: 100%"
         @current-change="handleCurrentChange"
       >
-        <el-table-column type="index" label="序号" width="50" />
-        <el-table-column property="tableName" label="表名" />
-        <el-table-column property="tableComment" label="表注释" />
-        <el-table-column property="dataSize" label="数据量" width="120" />
+        <el-table-column align="center" type="index" label="序号" width="50" />
+        <el-table-column property="name" label="表名" />
+        <el-table-column property="comment" label="表注释" />
+        <el-table-column align="center" property="dataSize" label="数据量" width="120" />
         <el-table-column property="isSelect" label="是否选择" width="120" />
       </el-table>
 
@@ -27,47 +27,29 @@
 
 <script setup>
 
-const currentRow = ref()
+const currentRow = ref(null)
 const singleTableRef = ref(null)
 
 const handleCurrentChange = (val) => {
   currentRow.value = val
 }
 
-const tableData = ref([
-  {
-    "tableName": "users",
-    "tableComment": "用户信息表",
-    "dataSize": 1000,
-    "isSelect": false
-  },
-  {
-    "tableName": "orders",
-    "tableComment": "订单信息表",
-    "dataSize": 5000,
-    "isSelect": false
-  },
-  {
-    "tableName": "products",
-    "tableComment": "产品信息表",
-    "dataSize": 2000,
-    "isSelect": false
-  },
-  {
-    "tableName": "departments",
-    "tableComment": "部门信息表",
-    "dataSize": 150,
-    "isSelect": false
-  }
-])
+const tableData = ref([])
 
 /** 获取当前行数据 */
 function handleSubmitCurrentRow(){
-  return currentRow.value.tableName
+  if(currentRow.value != null){
+    return currentRow.value.name
+  }
+  return null ;
+}
+
+function fillData(rows){
+  tableData.value = rows
 }
 
 // 暴露给父组件
-defineExpose({ handleSubmitCurrentRow })
+defineExpose({ fillData , handleSubmitCurrentRow })
 
 </script>
 

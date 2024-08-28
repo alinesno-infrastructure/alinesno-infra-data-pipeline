02b6bfc26cf89b77ffba0e537eca383342359a95
周健(James) <jamesAip@gmail.com>
2024-08-28 06:21:00 -0400

    改进JobQueueEntity类注释与添加相关注解和Lombok注解

--- alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/entity/JobQueueEntity.java
+++ alinesno-infra-data-pipeline-facade/src/main/java/com/alinesno/infra/data/pipeline/entity/JobQueueEntity.java
@@ -3,12 +3,18 @@ package com.alinesno.infra.data.pipeline.entity;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * TransferQueueEntity 类是任务列表实体类，用于表示任务的转移队列信息。
  * 它包含了转移名称、转移数据和状态等字段，用于记录任务的相关信息。
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 @TableName("pipeline_job_queue") // 指定数据库表名
+@Table(comment = "任务列表")
 public class JobQueueEntity extends InfraBaseEntity {
 
     /**
@@ -41,45 +47,4 @@ public class JobQueueEntity extends InfraBaseEntity {
     @TableField("step") // 指定数据库字段名
     private int step ;
 
-    // 省略 getter 和 setter 方法
-
-    public String getJobName() {
-        return jobName;
-    }
-
-    public void setJobName(String jobName) {
-        this.jobName = jobName;
-    }
-
-    public Long getJobId() {
-        return jobId;
-    }
-
-    public void setJobId(Long jobId) {
-        this.jobId = jobId;
-    }
-
-    public String getJobData() {
-        return jobData;
-    }
-
-    public void setJobData(String jobData) {
-        this.jobData = jobData;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public int getStep() {
-        return step;
-    }
-
-    public void setStep(int step) {
-        this.step = step;
-    }
 }

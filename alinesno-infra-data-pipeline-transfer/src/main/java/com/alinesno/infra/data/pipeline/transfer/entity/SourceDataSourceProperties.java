62cbf1113f9815ca4a2d48f81db6dbe997a2a47d
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:46 +0800

    feat:创建源端参数配置类SourceDataSourceProperties

---
+++ alinesno-infra-data-pipeline-transfer/src/main/java/com/alinesno/infra/data/pipeline/transfer/entity/SourceDataSourceProperties.java
@@ -0,0 +1,48 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.transfer.entity;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import lombok.Data;
+import com.alinesno.infra.data.pipeline.common.entity.PatternMapper;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+
+/**
+ * 源端参数配置
+ *
+ * @author tang
+ */
+@Data
+public class SourceDataSourceProperties {
+
+  private ProductTypeEnum type;
+  private String url;
+  private String driverClassName;
+  private String username;
+  private String password;
+  private String driverPath;
+  private Long connectionTimeout = TimeUnit.SECONDS.toMillis(60);
+  private Long maxLifeTime = TimeUnit.MINUTES.toMillis(60);
+
+  private String beforeSqlScripts;
+  private String afterSqlScripts;
+
+  private Integer fetchSize = 5000;
+  private String sourceSchema = "";
+  private String tableType = "TABLE";
+  private String sourceIncludes = "";
+  private String sourceExcludes = "";
+  private Map<String, String> incrTableColumns = Collections.emptyMap();
+  private List<PatternMapper> regexTableMapper = Collections.emptyList();
+  private List<PatternMapper> regexColumnMapper = Collections.emptyList();
+}

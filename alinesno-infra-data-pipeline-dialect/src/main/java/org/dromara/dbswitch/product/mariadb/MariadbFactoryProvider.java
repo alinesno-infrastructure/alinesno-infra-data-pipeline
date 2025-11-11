0662cabac8b138ccfd92a30b1beb5ee3a420d6e8
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:46 +0800

    feat:创建MariadbFactoryProvider类，实现MariaDB数据管道功能

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/org/dromara/dbswitch/product/mariadb/MariadbFactoryProvider.java
@@ -0,0 +1,52 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package org.dromara.dbswitch.product.mariadb;
+
+import com.alinesno.infra.data.pipeline.core.annotation.Product;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.core.features.ProductFeatures;
+import org.dromara.dbswitch.product.mysql.MysqlFeatures;
+import org.dromara.dbswitch.product.mysql.MysqlMetadataQueryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.AbstractFactoryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.meta.MetadataProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.AutoCastTableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.TableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.AutoCastTableDataWriteProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.TableDataWriteProvider;
+import javax.sql.DataSource;
+
+@Product(ProductTypeEnum.MARIADB)
+public class MariadbFactoryProvider extends AbstractFactoryProvider {
+
+  public MariadbFactoryProvider(DataSource dataSource) {
+    super(dataSource);
+  }
+
+  @Override
+  public ProductFeatures getProductFeatures() {
+    return new MysqlFeatures();
+  }
+
+  @Override
+  public MetadataProvider createMetadataQueryProvider() {
+    return new MysqlMetadataQueryProvider(this);
+  }
+
+  @Override
+  public TableDataWriteProvider createTableDataWriteProvider(boolean useInsert) {
+    return new AutoCastTableDataWriteProvider(this);
+  }
+
+  @Override
+  public TableDataSynchronizeProvider createTableDataSynchronizeProvider() {
+    return new AutoCastTableDataSynchronizeProvider(this);
+  }
+
+}

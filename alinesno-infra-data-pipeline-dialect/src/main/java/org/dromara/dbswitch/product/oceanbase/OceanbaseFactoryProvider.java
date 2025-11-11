62d5f0790487e9faef1f282def683f3584b202e4
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:47 +0800

    feat: 添加OceanbaseFactoryProvider类，实现OceanBase数据库的工厂提供者功能。

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/org/dromara/dbswitch/product/oceanbase/OceanbaseFactoryProvider.java
@@ -0,0 +1,96 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package org.dromara.dbswitch.product.oceanbase;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.core.annotation.Product;
+import com.alinesno.infra.data.pipeline.core.features.DefaultProductFeatures;
+import com.alinesno.infra.data.pipeline.core.features.ProductFeatures;
+import com.alinesno.infra.data.pipeline.core.provider.AbstractFactoryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.manage.DefaultTableManageProvider;
+import com.alinesno.infra.data.pipeline.core.provider.manage.TableManageProvider;
+import com.alinesno.infra.data.pipeline.core.provider.meta.MetadataProvider;
+import com.alinesno.infra.data.pipeline.core.provider.query.TableDataQueryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.AutoCastTableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.TableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.AutoCastTableDataWriteProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.TableDataWriteProvider;
+import org.dromara.dbswitch.product.mysql.MysqlFeatures;
+import org.dromara.dbswitch.product.mysql.MysqlMetadataQueryProvider;
+import org.dromara.dbswitch.product.oracle.OracleMetadataQueryProvider;
+import org.dromara.dbswitch.product.oracle.OracleTableDataSynchronizer;
+import org.dromara.dbswitch.product.oracle.OracleTableDataWriteProvider;
+import org.dromara.dbswitch.product.oracle.OracleTableManageProvider;
+
+@Slf4j
+@Product(ProductTypeEnum.OCEANBASE)
+public class OceanbaseFactoryProvider extends AbstractFactoryProvider {
+
+  private Boolean isMySqlMode;
+
+  public OceanbaseFactoryProvider(DataSource dataSource) {
+    super(dataSource);
+
+    try (Connection connection = getDataSource().getConnection()) {
+      this.isMySqlMode = OceanbaseUtils.isOceanBaseUseMysqlMode(connection);
+      if (log.isDebugEnabled()) {
+        log.debug("#### Target OceanBase is {} Mode ", this.isMySqlMode ? "MySQL" : "Oracle");
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public ProductFeatures getProductFeatures() {
+    return isMySqlMode ? new MysqlFeatures() : new DefaultProductFeatures();
+  }
+
+  @Override
+  public MetadataProvider createMetadataQueryProvider() {
+    MetadataProvider provider = isMySqlMode
+        ? new MysqlMetadataQueryProvider(this)
+        : new OracleMetadataQueryProvider(this);
+    return new OceanbaseMetadataQueryProvider(this, provider);
+  }
+
+  @Override
+  public TableDataQueryProvider createTableDataQueryProvider() {
+    return new OceanbaseTableDataQueryProvider(this, isMySqlMode);
+  }
+
+  @Override
+  public TableDataWriteProvider createTableDataWriteProvider(boolean useInsert) {
+    TableDataWriteProvider provider = isMySqlMode
+        ? new AutoCastTableDataWriteProvider(this)
+        : new OracleTableDataWriteProvider(this);
+    return new OceanbaseTableDataWriteProvider(this, provider);
+  }
+
+  @Override
+  public TableManageProvider createTableManageProvider() {
+    TableManageProvider provider = isMySqlMode
+        ? new DefaultTableManageProvider(this)
+        : new OracleTableManageProvider(this);
+    return new OceanbaseTableManageProvider(this, provider);
+  }
+
+  @Override
+  public TableDataSynchronizeProvider createTableDataSynchronizeProvider() {
+    TableDataSynchronizeProvider provider = isMySqlMode
+        ? new AutoCastTableDataSynchronizeProvider(this)
+        : new OracleTableDataSynchronizer(this);
+    return new OceanbaseTableDataSynchronizer(this, provider);
+  }
+}

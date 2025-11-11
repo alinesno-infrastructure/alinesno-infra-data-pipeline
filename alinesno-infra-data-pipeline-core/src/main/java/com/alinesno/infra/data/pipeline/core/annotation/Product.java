8b8a90129bd0c188095d319eb4c2a014798ccb38
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:47 +0800

    feat: 创建Product注解类，定义产品类型枚举字段

---
+++ alinesno-infra-data-pipeline-core/src/main/java/com/alinesno/infra/data/pipeline/core/annotation/Product.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.data.pipeline.core.annotation;
+
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Product {
+
+  ProductTypeEnum value();
+}

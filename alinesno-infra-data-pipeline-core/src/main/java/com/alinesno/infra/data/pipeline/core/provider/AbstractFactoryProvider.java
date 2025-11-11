fd7e766c81915e8ca504776ac7f7b1c291ed2430
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:48 +0800

    feat:创建AbstractFactoryProvider类，实现数据源和产品类型获取功能。

---
+++ alinesno-infra-data-pipeline-core/src/main/java/com/alinesno/infra/data/pipeline/core/provider/AbstractFactoryProvider.java
@@ -0,0 +1,42 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.core.provider;
+
+import com.alinesno.infra.data.pipeline.core.annotation.Product;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.common.util.ExamineUtils;
+import java.util.Objects;
+import javax.sql.DataSource;
+
+public abstract class AbstractFactoryProvider implements ProductFactoryProvider {
+
+  private DataSource dataSource;
+
+  protected AbstractFactoryProvider(DataSource dataSource) {
+    ExamineUtils.checkNotNull(dataSource, "datasource");
+    this.dataSource = dataSource;
+  }
+
+  @Override
+  public DataSource getDataSource() {
+    return this.dataSource;
+  }
+
+  @Override
+  public final ProductTypeEnum getProductType() {
+    Product annotation = getClass().getAnnotation(Product.class);
+    ExamineUtils.checkState(
+        Objects.nonNull(annotation),
+        "Should use Product annotation for class : %s",
+        getClass().getName());
+    return annotation.value();
+  }
+
+}

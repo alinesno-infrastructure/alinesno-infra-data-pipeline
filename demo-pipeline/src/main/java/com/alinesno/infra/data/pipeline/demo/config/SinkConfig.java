82e7c90aa0e0ae8d51b0d1b1c2c823bde04cb98e
王磊(Levi) <leviAip@gmail.com>
2025-11-12 00:37:48 +0800

    refactor: 使用Lombok简化SinkConfig类的getter和setter方法

--- demo-pipeline/src/main/java/com/alinesno/infra/data/pipeline/demo/config/SinkConfig.java
+++ demo-pipeline/src/main/java/com/alinesno/infra/data/pipeline/demo/config/SinkConfig.java
@@ -1,14 +1,10 @@
 package com.alinesno.infra.data.pipeline.demo.config;
 
+import lombok.Data;
+
+@Data
 public class SinkConfig {
     private String sinkName;
     private String table;
     private boolean upsert = true;
-
-    public String getSinkName() { return sinkName; }
-    public void setSinkName(String sinkName) { this.sinkName = sinkName; }
-    public String getTable() { return table; }
-    public void setTable(String table) { this.table = table; }
-    public boolean isUpsert() { return upsert; }
-    public void setUpsert(boolean upsert) { this.upsert = upsert; }
 }

ea783e6450e04e2ca890ab8590683b912616d5b5
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:29 +0800

    feat: 创建数据源创建页面，引入通用数据源组件

---
+++ alinesno-infra-data-pipeline-ui/src/views/data/pipeline/source/create.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- 传递 props 保持原有逻辑，Vue3 中 props 传递方式兼容 -->
+  <common-data-source :is-read-only="false"></common-data-source>
+</template>
+
+<script setup lang="ts">
+import CommonDataSource from './common.vue'
+</script>

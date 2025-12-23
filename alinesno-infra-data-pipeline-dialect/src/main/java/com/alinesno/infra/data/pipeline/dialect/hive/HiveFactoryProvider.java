23d95bca07e430ec4ee94f5103a7477a5f874eea
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:06 +0800

    feat:实现HiveFactoryProvider类，支持元数据和表数据查询功能。

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/hive/HiveFactoryProvider.java
@@ -0,0 +1,56 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.dialect.hive;
+
+import com.alinesno.infra.data.pipeline.core.annotation.Product;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.core.features.ProductFeatures;
+import com.alinesno.infra.data.pipeline.core.provider.AbstractFactoryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.meta.MetadataProvider;
+import com.alinesno.infra.data.pipeline.core.provider.query.TableDataQueryProvider;
+import javax.sql.DataSource;
+
+@Product(ProductTypeEnum.HIVE)
+public class HiveFactoryProvider extends AbstractFactoryProvider {
+
+  public HiveFactoryProvider(DataSource dataSource) {
+    super(dataSource);
+  }
+
+  @Override
+  public ProductFeatures getProductFeatures() {
+    return new HiveFeatures();
+  }
+
+  @Override
+  public MetadataProvider createMetadataQueryProvider() {
+    return new HiveMetadataQueryProvider(this);
+  }
+
+  @Override
+  public TableDataQueryProvider createTableDataQueryProvider() {
+    return new HiveTableDataQueryProvider(this);
+  }
+
+//  @Override
+//  public TableManageProvider createTableManageProvider() {
+//    throw new UnsupportedOperationException("Unsupported hive to manage tale!");
+//  }
+//
+//  @Override
+//  public TableDataWriteProvider createTableDataWriteProvider(boolean useInsert) {
+//    throw new UnsupportedOperationException("Unsupported hive to write tale data!");
+//  }
+//
+//  @Override
+//  public TableDataSynchronizeProvider createTableDataSynchronizeProvider() {
+//    throw new UnsupportedOperationException("Unsupported hive to sync tale data!");
+//  }
+}

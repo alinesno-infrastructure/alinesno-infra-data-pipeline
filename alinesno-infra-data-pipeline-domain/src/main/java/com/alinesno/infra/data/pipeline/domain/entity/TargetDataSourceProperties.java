04b883b0945c884f80112107ed73ae0b16a233ba
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:34 +0800

    feat: 创建目标数据源配置类 TargetDataSourceProperties

---
+++ alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/domain/entity/TargetDataSourceProperties.java
@@ -0,0 +1,49 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.domain.entity;
+
+import com.alinesno.infra.data.pipeline.common.type.CaseConvertEnum;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.common.type.SyncOptionEnum;
+import lombok.Data;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 目标端参数配置
+ *
+ * @author tang
+ */
+@Data
+public class TargetDataSourceProperties {
+
+  private ProductTypeEnum type;
+  private String url;
+  private String driverClassName;
+  private String username;
+  private String password;
+  private String driverPath;
+
+  private Long connectionTimeout = TimeUnit.SECONDS.toMillis(60);
+  private Long maxLifeTime = TimeUnit.MINUTES.toMillis(30);
+
+  private String targetSchema = "";
+  private CaseConvertEnum tableNameCase = CaseConvertEnum.NONE;
+  private CaseConvertEnum columnNameCase = CaseConvertEnum.NONE;
+  private Boolean targetDrop = Boolean.TRUE;
+  private Boolean onlyCreate = Boolean.FALSE;
+  private Boolean changeDataSync = Boolean.FALSE;
+  private Boolean createTableAutoIncrement = Boolean.FALSE;
+  private Boolean writerEngineInsert = Boolean.FALSE;
+
+  private String beforeSqlScripts;
+  private String afterSqlScripts;
+  private SyncOptionEnum targetSyncOption = SyncOptionEnum.INSERT_UPDATE_DELETE;
+}

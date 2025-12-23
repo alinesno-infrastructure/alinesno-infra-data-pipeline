1794dad4d6eb2128de650c92443ae644f279de7b
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:11 +0800

    feat: 创建 AssignmentDetailConverter 类，用于任务详情转换处理

---
+++ alinesno-infra-data-pipeline-gateway/src/main/java/com/alinesno/infra/data/pipeline/controller/converter/AssignmentDetailConverter.java
@@ -0,0 +1,82 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.controller.converter;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.alinesno.infra.data.pipeline.common.converter.AbstractConverter;
+import com.alinesno.infra.data.pipeline.dao.AssignmentConfigDAO;
+import com.alinesno.infra.data.pipeline.dao.DatabaseConnectionDAO;
+import com.alinesno.infra.data.pipeline.entity.AssignmentConfigEntity;
+import com.alinesno.infra.data.pipeline.entity.AssignmentTaskEntity;
+import com.alinesno.infra.data.pipeline.entity.DatabaseConnectionEntity;
+import com.alinesno.infra.data.pipeline.model.response.AssignmentDetailResponse;
+import com.alinesno.infra.data.pipeline.type.IncludeExcludeEnum;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class AssignmentDetailConverter extends
+        AbstractConverter<AssignmentTaskEntity, AssignmentDetailResponse> {
+
+  @Override
+  public AssignmentDetailResponse convert(AssignmentTaskEntity assignmentTaskEntity) {
+    DatabaseConnectionDAO databaseConnectionDAO = SpringUtil.getBean(DatabaseConnectionDAO.class);
+    AssignmentConfigDAO assignmentConfigDAO = SpringUtil.getBean(AssignmentConfigDAO.class);
+    AssignmentConfigEntity taskConfig = assignmentConfigDAO.getByAssignmentTaskId(
+        assignmentTaskEntity.getId());
+    DatabaseConnectionEntity srcConn = databaseConnectionDAO
+        .getById(taskConfig.getSourceConnectionId());
+    DatabaseConnectionEntity dstConn = databaseConnectionDAO
+        .getById(taskConfig.getTargetConnectionId());
+
+    AssignmentDetailResponse.Configuration config = new AssignmentDetailResponse.Configuration();
+    config.setSourceConnectionId(srcConn.getId());
+    config.setSourceConnectionName(srcConn.getName());
+    config.setSourceTypeName(srcConn.getType().getName());
+    config.setSourceSchema(taskConfig.getSourceSchema());
+    config.setTableType(taskConfig.getTableType());
+    config.setIncludeOrExclude(taskConfig.getExcludedFlag()
+        ? IncludeExcludeEnum.EXCLUDE
+        : IncludeExcludeEnum.INCLUDE);
+    config.setSourceTables(taskConfig.getSourceTables());
+    config.setIncrTableColumns(Optional.ofNullable(taskConfig.getIncrTableColumns()).orElseGet(ArrayList::new));
+    config.setSourceBeforeSqlScripts(taskConfig.getPreSqlScripts());
+    config.setSourceAfterSqlScripts(taskConfig.getPostSqlScripts());
+    config.setTargetConnectionId(dstConn.getId());
+    config.setTargetConnectionName(dstConn.getName());
+    config.setTargetTypeName(dstConn.getType().getName());
+    config.setTargetSchema(taskConfig.getTargetSchema());
+    config.setTargetOnlyCreate(taskConfig.getTargetOnlyCreate());
+    config.setTargetDropTable(taskConfig.getTargetDropTable());
+    config.setTargetAutoIncrement(taskConfig.getTargetAutoIncrement());
+    config.setTableNameCase(taskConfig.getTableNameCase());
+    config.setColumnNameCase(taskConfig.getColumnNameCase());
+    config.setTableNameMapper(Optional.ofNullable(taskConfig.getTableNameMap()).orElseGet(ArrayList::new));
+    config.setColumnNameMapper(Optional.ofNullable(taskConfig.getColumnNameMap()).orElseGet(ArrayList::new));
+    config.setBatchSize(taskConfig.getBatchSize());
+    config.setChannelSize(taskConfig.getChannelSize());
+    config.setTargetSyncOption(taskConfig.getTargetSyncOption());
+    config.setTargetBeforeSqlScripts(taskConfig.getBeforeSqlScripts());
+    config.setTargetAfterSqlScripts(taskConfig.getAfterSqlScripts());
+
+    AssignmentDetailResponse detailResponse = new AssignmentDetailResponse();
+    detailResponse.setId(assignmentTaskEntity.getId());
+    detailResponse.setName(assignmentTaskEntity.getName());
+    detailResponse.setDescription(assignmentTaskEntity.getDescription());
+    detailResponse.setScheduleMode(assignmentTaskEntity.getScheduleMode());
+    detailResponse.setCronExpression(assignmentTaskEntity.getCronExpression());
+    detailResponse.setCreateTime(assignmentTaskEntity.getCreateTime());
+    detailResponse.setUpdateTime(assignmentTaskEntity.getUpdateTime());
+    detailResponse.setConfiguration(config);
+
+    return detailResponse;
+  }
+
+}

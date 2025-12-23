0ab4a7915b60518ffe24603b380019bee63e32c1
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:39 +0800

    feat: 创建 ConnectionController 类，实现数据库连接相关接口

---
+++ alinesno-infra-data-pipeline-gateway/src/main/java/com/alinesno/infra/data/pipeline/controller/privateapi/ConnectionController.java
@@ -0,0 +1,146 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.controller.privateapi;
+
+import com.alinesno.infra.data.pipeline.common.annotation.LogOperate;
+import com.alinesno.infra.data.pipeline.common.annotation.TokenCheck;
+import com.alinesno.infra.data.pipeline.common.response.PageResult;
+import com.alinesno.infra.data.pipeline.common.response.Result;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.config.SwaggerConfig;
+import com.alinesno.infra.data.pipeline.model.request.DbConnectionCreateRequest;
+import com.alinesno.infra.data.pipeline.model.request.DbConnectionSearchRequest;
+import com.alinesno.infra.data.pipeline.model.request.DbConnectionUpdateRequest;
+import com.alinesno.infra.data.pipeline.model.response.DbConnectionDetailResponse;
+import com.alinesno.infra.data.pipeline.model.response.DbConnectionNameResponse;
+import com.alinesno.infra.data.pipeline.domain.ConnectionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@Api(tags = {"连接管理接口"})
+@RestController
+@RequestMapping(value = SwaggerConfig.API_V1 + "/connection")
+public class ConnectionController {
+
+  @Resource
+  private ConnectionService connectionService;
+
+  @TokenCheck
+  @ApiOperation(value = "数据库类型")
+  @GetMapping(value = "/types", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result getTypes() {
+    return Result.success(connectionService.getTypes());
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "数据库类型详情")
+  @GetMapping(value = "/type/{type}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result typeDetail(@PathVariable("type") ProductTypeEnum type) {
+    return Result.success(connectionService.getTypeDetail(type));
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "数据库类型")
+  @GetMapping(value = "/{type}/drivers", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result getDrivers(@PathVariable("type") ProductTypeEnum type) {
+    return Result.success(connectionService.getDrivers(type));
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "连接列表")
+  @PostMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+  public PageResult<DbConnectionDetailResponse> getConnections(@RequestBody DbConnectionSearchRequest request) {
+    return connectionService.getConnections(request);
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "连接详情")
+  @GetMapping(value = "/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result<DbConnectionDetailResponse> getDetail(@PathVariable("id") Long id) {
+    return Result.success(connectionService.getDetailById(id));
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "测试连接")
+  @GetMapping(value = "/test/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result test(@PathVariable("id") Long id) {
+    return connectionService.test(id);
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "预测试连接")
+  @PostMapping(value = "/preTest", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result preTest(@RequestBody DbConnectionCreateRequest request) {
+    return connectionService.preTest(request);
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "查询连接的Schema列表")
+  @GetMapping(value = "/schemas/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result getSchemas(@PathVariable("id") Long id) {
+    return connectionService.getSchemas(id);
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "查询连接在制定Schema下的所有物理表列表")
+  @GetMapping(value = "/tables/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result getSchemaTables(@PathVariable("id") Long id,
+      @RequestParam("schema") String schema) {
+    return connectionService.getSchemaTables(id, schema);
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "查询连接在制定Schema下的所有视图表列表")
+  @GetMapping(value = "/views/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result getSchemaViews(@PathVariable("id") Long id,
+      @RequestParam("schema") String schema) {
+    return connectionService.getSchemaViews(id, schema);
+  }
+
+  @TokenCheck
+  @LogOperate(name = "数据库连接", description = "'添加的数据库连接标题为：'+#request.name")
+  @ApiOperation(value = "添加连接")
+  @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result<DbConnectionDetailResponse> create(@RequestBody DbConnectionCreateRequest request) {
+    return connectionService.addDatabaseConnection(request);
+  }
+
+  @TokenCheck
+  @LogOperate(name = "数据库连接", description = "'修改的数据库连接ID为：'+#request.id")
+  @ApiOperation(value = "修改连接")
+  @PostMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result update(@RequestBody DbConnectionUpdateRequest request) {
+    return connectionService.updateDatabaseConnection(request);
+  }
+
+  @TokenCheck
+  @LogOperate(name = "数据库连接", description = "'删除的数据库连接ID为：'+#id")
+  @ApiOperation(value = "删除连接")
+  @DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Result delete(@PathVariable("id") Long id) {
+    connectionService.deleteDatabaseConnection(id);
+    return Result.success();
+  }
+
+  @TokenCheck
+  @ApiOperation(value = "连接名称")
+  @GetMapping(value = "/list/name", produces = MediaType.APPLICATION_JSON_VALUE)
+  public PageResult<DbConnectionNameResponse> getNameList(
+      @RequestParam(value = "page", required = false) Integer page,
+      @RequestParam(value = "size", required = false) Integer size) {
+    return connectionService.getNameList(page, size);
+  }
+
+}

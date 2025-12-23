4850df85e5ac8517afd8558a43f5b91213311319
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:08:53 +0800

    更新:alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/mapper/IJobQueueMapper.java

--- alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/mapper/IJobQueueMapper.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.data.pipeline.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.pipeline.entity.JobQueueEntity;
-
-public interface IJobQueueMapper extends IBaseMapper<JobQueueEntity> {
-}

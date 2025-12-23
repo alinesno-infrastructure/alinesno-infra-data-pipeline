db0c74d2ad01fdfb41921ec182b8334c411cabb0
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:18 +0800

    feat: 创建 IPgBulkInsert 接口，定义批量插入数据的方法。

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/postgresql/copy/IPgBulkInsert.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.pipeline.dialect.postgresql.copy;
+
+import java.sql.SQLException;
+import java.util.stream.Stream;
+import org.postgresql.PGConnection;
+
+public interface IPgBulkInsert<TEntity> {
+
+  void saveAll(PGConnection connection, Stream<TEntity> entities) throws SQLException;
+}

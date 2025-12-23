b4cd8e03d7eefe093638bf399a412693f3844755
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:45 +0800

    feat: 创建FrontendEntity类，定义前端节点相关属性

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/startrocks/FrontendEntity.java
@@ -0,0 +1,22 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.dialect.startrocks;
+
+import lombok.Data;
+
+@Data
+public class FrontendEntity {
+
+  private String ip;
+  private String httpport;
+  private Boolean alive;
+  private Boolean join;
+  private String role;
+}

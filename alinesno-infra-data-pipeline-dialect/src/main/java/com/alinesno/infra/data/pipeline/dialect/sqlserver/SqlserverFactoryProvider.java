4b5b4f06d59d8589e9ee0f92455c5a045fa59658
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:10 +0800

    feat: 添加SQL Server工厂提供者类SqlserverFactoryProvider

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/sqlserver/SqlserverFactoryProvider.java
@@ -0,0 +1,58 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.dialect.sqlserver;
+
+import com.alinesno.infra.data.pipeline.core.annotation.Product;
+import com.alinesno.infra.data.pipeline.common.type.ProductTypeEnum;
+import com.alinesno.infra.data.pipeline.core.features.DefaultProductFeatures;
+import com.alinesno.infra.data.pipeline.core.features.ProductFeatures;
+import com.alinesno.infra.data.pipeline.core.provider.AbstractFactoryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.meta.MetadataProvider;
+import com.alinesno.infra.data.pipeline.core.provider.manage.DefaultTableManageProvider;
+import com.alinesno.infra.data.pipeline.core.provider.manage.TableManageProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.AutoCastTableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.sync.TableDataSynchronizeProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.AutoCastTableDataWriteProvider;
+import com.alinesno.infra.data.pipeline.core.provider.write.TableDataWriteProvider;
+import javax.sql.DataSource;
+
+@Product(ProductTypeEnum.SQLSERVER)
+public class SqlserverFactoryProvider extends AbstractFactoryProvider {
+
+  public SqlserverFactoryProvider(DataSource dataSource) {
+    super(dataSource);
+  }
+
+  @Override
+  public ProductFeatures getProductFeatures() {
+    return new DefaultProductFeatures();
+  }
+
+  @Override
+  public MetadataProvider createMetadataQueryProvider() {
+    return new SqlserverMetadataQueryProvider(this);
+  }
+
+  @Override
+  public TableDataWriteProvider createTableDataWriteProvider(boolean useInsert) {
+    return new AutoCastTableDataWriteProvider(this);
+  }
+
+  @Override
+  public TableManageProvider createTableManageProvider() {
+    return new DefaultTableManageProvider(this);
+  }
+
+  @Override
+  public TableDataSynchronizeProvider createTableDataSynchronizeProvider() {
+    return new AutoCastTableDataSynchronizeProvider(this);
+  }
+
+}

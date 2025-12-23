00a9e74fe5b2ccd11790bdfc842ac1afac820f28
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:26 +0800

    feat: 创建SystemUserMapper接口，继承BaseMapper

---
+++ alinesno-infra-data-pipeline-gateway/src/main/java/com/alinesno/infra/data/pipeline/mapper/SystemUserMapper.java
@@ -0,0 +1,17 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.mapper;
+
+import com.alinesno.infra.data.pipeline.entity.SystemUserEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface SystemUserMapper extends BaseMapper<SystemUserEntity> {
+
+}

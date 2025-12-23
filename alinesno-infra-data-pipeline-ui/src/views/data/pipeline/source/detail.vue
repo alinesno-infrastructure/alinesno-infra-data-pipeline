eec6042543969c7610ccaf27ea7f89e36d172db1
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:16 +0800

    feat: 创建数据源详情页面组件，引入只读模式的公共数据源组件

---
+++ alinesno-infra-data-pipeline-ui/src/views/data/pipeline/source/detail.vue
@@ -0,0 +1,11 @@
+<template>
+  <commonDataSource :isReadOnly="true"></commonDataSource>
+</template>
+
+<script setup>
+// 导入组件（Vue3 中组件命名建议使用 PascalCase 规范）
+import CommonDataSource from './common'
+</script>
+
+<style scoped>
+</style>

509cb1adf21d5e8534e45ac9fd17bcc9f3db43db
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:08:56 +0800

    feat: 创建LogbackEventContent类，包含identity和content属性

---
+++ alinesno-infra-data-pipeline-gateway/src/main/java/com/alinesno/infra/data/pipeline/logback/LogbackEventContent.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.pipeline.logback;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogbackEventContent {
+
+  private String identity;
+  private String content;
+}

28640ea0f8cd00b6e458807735d2fb8aa8a0254b
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:14 +0800

    feat: 创建DB2TableManageProvider类，实现截断表数据功能

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/db2/DB2TableManageProvider.java
@@ -0,0 +1,28 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.dialect.db2;
+
+
+import com.alinesno.infra.data.pipeline.core.provider.ProductFactoryProvider;
+import com.alinesno.infra.data.pipeline.core.provider.manage.DefaultTableManageProvider;
+
+public class DB2TableManageProvider extends DefaultTableManageProvider {
+
+  public DB2TableManageProvider(ProductFactoryProvider factoryProvider) {
+    super(factoryProvider);
+  }
+
+  @Override
+  public void truncateTableData(String schemaName, String tableName) {
+    String sql = String.format("TRUNCATE TABLE \"%s\".\"%s\" IMMEDIATE ", schemaName, tableName);
+    this.executeSql(sql);
+  }
+  
+}

31ce54a7f13b16fc2b55de7f95f7c07a1eadca5f
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:13 +0800

    feat: 创建Point类，包含x和y坐标及构造方法

---
+++ alinesno-infra-data-pipeline-dialect/src/main/java/com/alinesno/infra/data/pipeline/dialect/postgresql/copy/pgsql/model/geometric/Point.java
@@ -0,0 +1,21 @@
+package com.alinesno.infra.data.pipeline.dialect.postgresql.copy.pgsql.model.geometric;
+
+public class Point {
+
+  private final double x;
+  private final double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+}

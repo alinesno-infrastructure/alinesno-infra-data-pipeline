1131032c56c8c9c5b1b0bcddb8deebf2bb7e7852
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:08:52 +0800

    更新:alinesno-infra-data-pipeline-dialect/src/main/java/org/dromara/dbswitch/product/postgresql/copy/pgsql/handlers/IValueHandlerProvider.java

--- alinesno-infra-data-pipeline-dialect/src/main/java/org/dromara/dbswitch/product/postgresql/copy/pgsql/handlers/IValueHandlerProvider.java
+++
@@ -1,8 +0,0 @@
-package org.dromara.dbswitch.product.postgresql.copy.pgsql.handlers;
-
-import org.dromara.dbswitch.product.postgresql.copy.pgsql.constants.DataType;
-
-public interface IValueHandlerProvider {
-
-  <TTargetType> IValueHandler<TTargetType> resolve(DataType targetType);
-}

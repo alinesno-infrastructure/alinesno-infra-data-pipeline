324e3cfb059f86ab459b52ad426f8b06153b5d9a
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:41 +0800

    feat: 创建 ComparePerfStat 类，用于比较性能统计信息的格式化输出

---
+++ alinesno-infra-data-pipeline-domain/src/main/java/com/alinesno/infra/data/pipeline/domain/domain/ComparePerfStat.java
@@ -0,0 +1,48 @@
+// Copyright tang.  All rights reserved.
+// https://gitee.com/inrgihc/dbswitch
+//
+// Use of this source code is governed by a BSD-style license
+//
+// Author: tang (inrgihc@126.com)
+// Date : 2020/1/2
+// Location: beijing , china
+/////////////////////////////////////////////////////////////
+package com.alinesno.infra.data.pipeline.domain.domain;
+
+import com.alinesno.infra.data.pipeline.common.entity.PrintablePerfStat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * 比较信息格式化输出
+ *
+ * @author tang
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComparePerfStat extends PrintablePerfStat {
+
+  private Map<String, Long> readMap;
+
+  private Map<String, Long> writeMap;
+
+  @Override
+  public String getPrintableString() {
+    StringBuilder sb = new StringBuilder();
+    if (readMap.size() > 0) {
+      sb.append("Table Detail Information Follows:\n");
+      for (Map.Entry<String, Long> entry : readMap.entrySet()) {
+        String tableMapName = entry.getKey();
+        Long tableReadTotal = entry.getValue();
+        Long tableWriteTotal = writeMap.getOrDefault(tableMapName, 0L);
+        sb.append("  " + tableMapName + " [read: " + tableReadTotal + ", write:" + tableWriteTotal + "] \n");
+      }
+    }
+    return sb.toString();
+  }
+
+}

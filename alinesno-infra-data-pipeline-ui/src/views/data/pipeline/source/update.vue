7b8d888eb7f8eaf15b8f8acce7afa91ddd3fcf0e
张婷(Tina) <tinaAip@gmail.com>
2025-12-23 08:09:17 +0800

    feat: 创建数据源更新页面，引入通用数据源组件

---
+++ alinesno-infra-data-pipeline-ui/src/views/data/pipeline/source/update.vue
@@ -0,0 +1,14 @@
+<template>
+  <commonDataSource :isReadOnly="isReadOnly"></commonDataSource>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CommonDataSource from './common'
+
+// 示例：添加响应式数据（如需扩展功能时使用）
+const isReadOnly = ref(false)
+</script>
+
+<style scoped>
+</style>
